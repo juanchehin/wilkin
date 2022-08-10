958bfd9b688d0a40c621eac2c00268dbec4c9223
Juan Chehin <chehinjuan@gmail.com>
2022-08-10 12:12:32 -0300

    install packages

--- front-wilkin/src/app/app.module.ts
+++ front-wilkin/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+// Rutas
+import { APP_ROUTES } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './shared/login/login.component';
 
+import { ServiceModule } from './services/service.module';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule
+    APP_ROUTES,
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
